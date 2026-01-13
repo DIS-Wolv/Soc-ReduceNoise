91eca7f093103a7edc07fbdf833f4a3d168a531a
I-nnana <120049177+I-nnana@users.noreply.github.com>
2026-01-13 23:14:26 +0100

    hostname isolation

--- index.ts
+++ index.ts
@@ -7,14 +7,20 @@ try {
 	const data = fs.readFileSync(filePath, 'utf-8');
 
 	// split string to items based on \n character
-	const logsSplitted = data.split("\n");
+	const splittingLogs = data.split("\n");
 
+    const reFetchHostname : RegExp = /^(?:\S+\s+){3}(\S+)/;
 
-
-	logsSplitted.forEach((event: string) => {
-		console.log("======>: "+ event);
-	});
-
+    splittingLogs.forEach((item) => {
+        const match = item.match(reFetchHostname)
+        if (match) {
+            // match[1] contient le hostname capturé
+            console.log("Hostname :", match[1]);
+        } else {
+            // Pas de correspondance – la ligne n’est peut‑être pas au format syslog attendu
+            console.log("Pas de hostname trouvé pour :", item);
+        }
+    });
 
 } catch (error) {
 	console.error('Error reading file:', error);
